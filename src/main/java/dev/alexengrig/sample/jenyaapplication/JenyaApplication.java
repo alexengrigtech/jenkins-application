e6180d74dafc13e74994860b481b5e3e47e98f2c
Grig Alex <alexengrigdev@gmail.com>
2022-12-03 12:25:06 +0300

    Init spring boot project

---
+++ src/main/java/dev/alexengrig/sample/jenyaapplication/JenyaApplication.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.sample.jenyaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/alexengrig/sample/jenyaapplication/JenyaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.sample.jenyaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
